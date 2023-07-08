8704f1346d7c23f764f3056cd8e00465ce369f82
takanashi11 <132178818+takanashi11@users.noreply.github.com>
2023-07-08 12:47:44 +0900

    コメント機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,7 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   belongs_to_active_hash :category
+  has_many :comments
 
   with_options presence: true do
     validates :image
